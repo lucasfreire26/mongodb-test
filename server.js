1e294995e4a105069ba98ed7aae2f8fb0e3b6b0a
lucasfreire26 <lucascipre@gmail.com>
2020-03-12 11:16:06 +0000

    week4final

---
+++ server.js
@@ -0,0 +1,28 @@
+var logger = require("morgan"),
+cors = require("cors"),
+http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser"),
+mongoose = require('mongoose');
+
+var app = express();
+var port = 3000;
+var userCtrl = require('./user-controller');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(require('./routes'));
+
+app.get('/users', userCtrl.createUser);
+app.listen(port, function(err){
+    console.log("Listening on Port: " + port)
+});
+
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
